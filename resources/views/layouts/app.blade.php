219e23244743010a98ed508c2d4f32c0566ea775
yofan2408 <yofan.ixe@gmail.com>
2022-07-28 13:08:10 +0700

    fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -43,11 +43,11 @@
         </footer>
     </div>
 
-    <script id="__bs_script__">
+    {{-- <script id="__bs_script__">
         //<![CDATA[
         document.write("<script async src='http://HOST:3000/browser-sync/browser-sync-client.js?v=2.27.9'><\/script>"
             .replace("HOST", location.hostname));
         //]]>
-    </script>
+    </script> --}}
 </body>
 </html>
